25ca5907a099b9904fb0c58710cb7e9acd5731dc
Luis Larrea <luis.larrea@gmail.com>
2017-02-17 03:50:43 +0000

    Clear errors and add SASS support

--- config/environment.js
+++ config/environment.js
@@ -10,7 +10,8 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-      }
+      },
+      EXTEND_PROTOTYPES: { Date: false }
     },
 
     APP: {
@@ -24,7 +25,31 @@ module.exports = function(environment) {
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;// config/environment.js
+    ENV.contentSecurityPolicy = {
+      // Deny everything by default
+      'default-src': "'none'",
+
+      // Allow scripts at https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js
+      'script-src': ["'self' 'unsafe-inline' 'unsafe-eval'", "https://platform.twitter.com http://connect.facebook.net https://cdn.mxpnl.com https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js"],
+
+      // Allow fonts to be loaded from http://fonts.gstatic.com
+      'font-src': ["'self'", "http://fonts.gstatic.com"],
+
+      // Allow data (xhr/websocket) from api.mixpanel.com and custom-api.local
+      'connect-src': ["'self'", "https://api.mixpanel.com https://custom-api.local"],
+
+      // Allow images from the origin itself (i.e. current domain)
+      'img-src': ["'self'", "https://www.facebook.com"],
+
+      // Allow CSS loaded from https://fonts.googleapis.com
+      'style-src': ["'self' 'unsafe-inline' 'unsafe-eval'", "https://fonts.googleapis.com"],
+      'frame-src': ["https://staticxx.facebook.com http://staticxx.facebook.com s-static.ak.facebook.com static.ak.facebook.com www.facebook.com"],
+
+      // Omit `media-src` from policy
+      // Browser will fallback to default-src for media resources (which is 'none', see above)
+      'media-src': null
+    }
   }
 
   if (environment === 'test') {
